6583564fb77853de32ea5c02c4d553b2c2de7455
denisgao <denis@electricenjin.com>
2018-04-10 10:57:27 -0400

    Found two amazing page transitions using svg to learn from. Basic animations on first page.

---
+++ src/components/start.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="container relative">
+    <div class="card">
+      <img class="image" src="static/bohoturtle.svg">
+    </div>
+    <div @mouseover="dostuff"  class="bottom note absolute">
+      <p>SVG can swim</p>
+    </div>
+  </div>
+</template>
+
+<script>
+  var moveTurtle = function(){
+    TweenMax.to(jQuery('.image'),1,{opacity:0})
+  }
+  export default {
+    name: 'test',
+    methods:{
+      dostuff : function(){
+        console.log("HELLO");
+        moveTurtle();
+      },
+      // moveTurtle : function(){
+      // }
+    }
+  }
+
+
+</script>
+<style>
+  .card{
+    max-width: 800px;
+    padding:0 20px;
+  }
+  .image{
+    width:100%;
+    height: 100vh;
+  }
+  .note{
+    max-width: 200px;
+    text-center:center;
+    padding: 0 20px;
+    border: 2px black double;
+    transition:all 1s ease-out;
+  }
+  .note:hover{
+    transform:translateX(30px);
+    opacity: 0;
+  }
+  .note >  p{
+    font-family: Helvetica;
+    font-size:14px;
+  }
+  .bottom{
+    bottom:20%;
+    right:10%;
+  }
+</style>
+
